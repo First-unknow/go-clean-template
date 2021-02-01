a09248481eade373bf31bb725d2902c5d78adfa6
first-unknow <firstpan555@gmail.com>
2021-02-01 16:47:21 +0700

    👾 update implement FindUserByMail and Get data from database in repository

--- service/user/repository/psql_users.go
+++ service/user/repository/psql_users.go
@@ -47,6 +47,33 @@ func (p psqlUserRepository) FetchAll() ([]*models.User, error) {
 	return users, nil
 }
 
+func (p psqlUserRepository) FindByMail(email string) ([]*models.User, error) {
+	sql := fmt.Sprintf(`
+		SELECT 
+			%s
+		FROM users
+		WHERE users.email='%s'
+	`,
+		models.UserSelector,
+		email,
+	)
+
+	log.Println(sql)
+
+	rows, err := p.db.Queryx(sql)
+	log.Println(rows)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users, err := p.orm(rows, nil)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
 func (p psqlUserRepository) CreateUser(user *models.User) (uuid.UUID, error) {
 	var userId uuid.UUID
 	tx, err := p.db.Begin()
