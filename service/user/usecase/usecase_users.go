9e9b66c899dcd286a9822530d640b04a71868680
first-unknow <firstpan555@gmail.com>
2021-02-01 15:45:27 +0700

    👾 update implement interface CreateUser in user/usecase

--- service/user/usecase/usecase_users.go
+++ service/user/usecase/usecase_users.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	models "innovasive/go-clean-template/models"
 	"innovasive/go-clean-template/service/user"
+
+	"github.com/google/uuid"
 )
 
 type userUsecase struct {
@@ -17,4 +19,8 @@ func NewUserUsecase(uRepo user.PsqlUserRepositoryInf) user.UserUsecaseInf {
 
 func (u userUsecase) FetchAll() ([]*models.User, error) {
 	return u.psqlUserRepo.FetchAll()
+}
+
+func (u userUsecase) CreateUser(user *models.User) (uuid.UUID, error) {
+	return u.psqlUserRepo.CreateUser(user)
 }
